2c2c379e9a71a6ad95863d2f30a0970d31e6a31b
María Camila Patarroyo <72886478+Camila-pv@users.noreply.github.com>
2022-08-15 09:56:26 -0500

    Add files via upload

--- gramaticaKarel.cpp
+++ gramaticaKarel.cpp
@@ -7,7 +7,7 @@
 bool gramatica(std::vector<std::string> &myvec);
 bool gram_start(std::vector<std::string> &myvec);
 bool gram_program(std::vector<std::string> &myvec);
-//bool gram_definition(std::vector<std::string> &myvec);
+bool gram_definition(std::vector<std::string> &myvec);
 
 /**
 bool gram_statement(std::vector<std::string> myvec);
@@ -42,15 +42,15 @@ bool instruction(std::vector<std::string> myvec);
 
 int main(){
     std::vector<std::string> myvec;
-    
+    int count = 0;
     std::fstream newfile;
   //Leer el programa fuente
   newfile.open("prueba.txt",std::ios::in); //open a file to perform read operation using file object
    if (newfile.is_open()){   //checking whether the file is open
      std::string tp;
       while(getline(newfile, tp)){  //read data from file object and put it into string.
-	//Llamar la función
-        myvec.push_back(tp);  
+	//Llamar la funció
+	myvec.push_back(tp);
       }
       newfile.close();   //close the file object.
    }
@@ -65,19 +65,26 @@ int main(){
     return 0;
 }
 
+// LLama a la función gram_start y le pasa el vector
 bool gramatica(std::vector<std::string> &myvec){
   return gram_start(myvec);
 }
 
 
-// ESPACION AL INICIO, AL FINAL Y EN EL MEDIO DE TODO EL CODIGO
+// POR HACER : ESPACION AL INICIO, AL FINAL Y EN EL MEDIO DE TODO EL CODIGO
+// Identifica BEGINNING-OF-PROGRAM y END-OF-PROGRAM
+// Luego llama a la función gram_program si sigue la ejecucion del programa
+// si empieza la creación de una instrucción llama a gram_definition
 bool gram_start(std::vector<std::string> &myvec){
   std::vector<std::string> myvec2;
   if(std::regex_match(myvec[0], std::regex("(BEGINNING-OF-PROGRAM)([\\s]*)")) && std::regex_match(myvec[myvec.size()-1],std::regex("(END-OF-PROGRAM)([\\s]*)"))){
     for(int i = 1; i < myvec.size()-1; i++)
       myvec2.push_back(myvec[i]);
     myvec = myvec2;
-    return gram_program(myvec);
+    if(std::regex_match(myvec[0], std::regex("([\\s]*)(BEGINNING−OF−EXECUTION)([\\s]*)")))
+      return gram_program(myvec);
+    else
+      return gram_definition(myvec);
   }else{
     return false;
   }
@@ -85,7 +92,8 @@ bool gram_start(std::vector<std::string> &myvec){
 
 
 
-// TABULACION
+// POR HACER : TABULACION
+// Identifica BEGINNING-OF-EXECUTION y END-OF-EXECUTION
 bool gram_program(std::vector<std::string> &myvec){
   std::vector<std::string> myvec2;
   if(std::regex_match(myvec[0], std::regex("([\\s]*)(BEGINNING−OF−EXECUTION)([\\s]*)")) && std::regex_match(myvec[myvec.size()-1],std::regex("([\\s]*)(END-OF-EXECUTION)([\\s]*)"))){
@@ -99,14 +107,18 @@ bool gram_program(std::vector<std::string> &myvec){
 }
 
 
-
-
-
-
-
-/**
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Identifica la creación de una instrucción
 bool gram_definition(std::vector<std::string> &myvec){
-  std::cout<<"Nueva funcion\n";
+  std::vector<std::string> myvec2;
+  if(std:: regex_match(myvec[0], std::regex("([\\s]*)(DEFINE-NEW-INSTRUCTION)([\\s]*)([A-Za-z])*([\\s]*)(AS)([\\s]*)"))){
+    for(int i = 1;i < myvec.size(); i++)
+      myvec2.push_back(myvec[i]);
+    myvec= myvec2;   
+    std::cout<<"Nueva funcioon\n";
   return true;
+   
+  }
+  return false;
 }
-**/
+///////////////////////////////////////////////////////////////////////////////////////
